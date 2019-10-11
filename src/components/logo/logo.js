8b1f182b8dd51c7068f7b471cc0e69e4a28db56b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-10-11 10:32:21 +0530

    updated logo design and styling

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Tilt from "react-tilt";
 import "./logo.css";
+import ai from "./ai.png";
 
 const Logo = () => {
   return (
     <Tilt
       className="Tilt br2 shadow-2"
-      options={{ max: 25 }}
-      style={{ height: 250, width: 250 }}
+      options={{ max: 50 }}
+      style={{ height: 150, width: 150 }}
     >
-      <div className="Tilt-inner">👽</div>
+      <div className="Tilt-inner pa3">
+        <img src={ai} alt="logo" style={{ paddingRight: "2px" }} />
+      </div>
     </Tilt>
   );
 };
