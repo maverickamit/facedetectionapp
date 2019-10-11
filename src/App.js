da9c216308f08faede81f3aa9ef77ecc8847f4f4
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-10-11 22:25:02 +0530

    added rank component and pointer background

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import Navigation from "./components/navigation/navigation";
 import Logo from "./components/logo/logo";
 import InputForm from "./components/inputform/inputform";
+import Rank from "./components/rank/rank";
 
+import Particles from "react-particles-js";
 import Tachyons from "tachyons/css/tachyons.min.css";
+import Params from "./pointers";
 
 function App() {
   return (
     <div className="App">
+      <Particles params={Params} className="particles" />
       <Navigation />
       <Logo />
+      <Rank />
       <InputForm />
+
       {/* <facedetection /> */}
     </div>
   );
---
+++ src/components/rank/rank.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Rank = () => {
+  return <h3>Tom, you are ranked #5! </h3>;
+};
+
+export default Rank;
