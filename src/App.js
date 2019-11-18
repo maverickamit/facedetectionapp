d0faa698d427daf91675b7d512410b6d04fbb571
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-11-18 23:52:24 +0530

    implemented count increasing with each click on submit

--- src/App.js
+++ src/App.js
@@ -61,7 +61,22 @@ class App extends Component {
   };
 
   onButtonSubmit = event => {
-    console.log("click");
+    fetch("http://localhost:3000/image", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: this.state.user.id
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          user: {
+            ...this.state.user,
+            entries: data
+          }
+        });
+      });
 
     this.setState({ imageUrl: this.state.input });
     app.models
